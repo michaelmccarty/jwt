09c5e3a09ea08693e8d91f6b6734d5ac12445ab4
michaelmccarty <mccartyatl@gmail.com>
2019-09-21 17:35:30 -0400

    .

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,8 @@ module.exports = function(app, db) {
   app.post("/login", function(req, res) {
     let username = req.body.username;
     let password = req.body.password;
+
+    
     console.log(req.body.username);
 
     res.end();
